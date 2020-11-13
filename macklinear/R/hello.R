88f88afdb4c30d7050efb4be911e8339be91ad16
kaefogelman <kjf0021@auburn.edu>
2020-11-12 21:28:11 -0600

    create vignette rmd as part of package

--- macklinear/R/hello.R
+++ macklinear/R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -13,6 +13,10 @@
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
 
+##code to create vignette components https://r-pkgs.org/vignettes.html#vignette-workflow
+usethis::use_vignette("my-vignette")
+
 hello <- function() {
   print("Hello, world!")
 }
+
