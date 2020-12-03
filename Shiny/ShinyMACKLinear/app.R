e1f5208b3545c1b188be3567b2932045649792cd
Maria Tereza Bethonico Terra <mzb0129@auburn.edu>
2020-12-03 15:15:01 -0600

    changed install to library

--- Shiny/ShinyMACKLinear/app.R
+++ Shiny/ShinyMACKLinear/app.R
@@ -12,7 +12,8 @@ library(shiny)
 library(shinythemes)
 library(r2symbols)
 library(shinyjs)
-install.packages('rsconnect')
+library(rsconnect)
+
 # Define UI
 ui <- fluidPage(theme = shinytheme("cosmo"),
                 
@@ -64,7 +65,7 @@ ui <- fluidPage(theme = shinytheme("cosmo"),
 
                                  #ABA2
                               
-                                tabPanel("Meet the team", style="text-align:center;", 
+                                tabPanel("Meet the team", style="text-align:center;", style = "border: 4px double grey;",
                                          
          
                                                   
@@ -99,7 +100,40 @@ ui <- fluidPage(theme = shinytheme("cosmo"),
 
          br(),
          br(),
+        
          
+         # Define UI for data download app ----
+         ui <- fluidPage(
+           
+           # App title ----
+           titlePanel("Downloading Data"),
+           
+           # Sidebar layout with input and output definitions ----
+           sidebarLayout(
+             
+             # Sidebar panel for inputs ----
+             sidebarPanel(
+               
+               # Input: Choose dataset ----
+               selectInput("dataset", "Choose a dataset:",
+                           choices = c("rock", "pressure", "cars")),
+               
+               # Button
+               downloadButton("downloadData", "Download")
+               
+             ),
+             
+             # Main panel for displaying outputs ----
+             mainPanel(
+               
+               tableOutput("table")
+               
+           
+             
+           )
+         ),
+         
+   
          
          hr(),
          br(),
@@ -108,6 +142,7 @@ ui <- fluidPage(theme = shinytheme("cosmo"),
 )
 )
 #As tres ou duas ultimas de sempre que cada hora eh uma coisa
+)
 )
 )
 
@@ -120,7 +155,40 @@ server <- function(input, output) {
     # output$txtout <- renderText({
     #     paste( input$txt1, input$txt2, sep = " " )
     # })
-} # server
+
+
+    
+    # Reactive value for selected dataset ----
+    datasetInput <- reactive({
+      switch(input$dataset,
+             "rock" = rock,
+             "pressure" = pressure,
+             "cars" = cars)
+    })
+    
+    # Table of selected dataset ----
+    output$table <- renderTable({
+      datasetInput()
+    })
+    
+    # Downloadable csv of selected dataset ----
+    output$downloadData <- downloadHandler(
+      filename = function() {
+        paste(input$dataset, ".csv", sep = "")
+      },
+      content = function(file) {
+        write.csv(datasetInput(), file, row.names = FALSE)
+      }
+    )
+    
+  
+  
+  
+  } # server
+
+
+
+
 
 
 # Create Shiny object
