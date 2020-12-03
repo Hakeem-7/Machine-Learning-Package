6e27d28ea17bddbb78b18a699c83c0b3c5089090
kaefogelman <kjf0021@auburn.edu>
2020-12-03 13:11:47 -0600

    update run_shiny for our package

--- macklinear/R/run_shiny.R
+++ macklinear/R/run_shiny.R
@@ -1,5 +1,5 @@
 #' @export
 MC_gui = function(){
-  appDir = system.file("MC_int", package = "demo")
+  appDir = system.file("my_lm", package = "macklinear")
   shiny::runApp(appDir, display.mode = "normal")
 }
