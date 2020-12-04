91058f3efb08ee5d8111768146211b257fe034a4
Maria Tereza Bethonico Terra <mzb0129@auburn.edu>
2020-12-04 13:38:02 -0600

    Trying out the App Function

--- Shiny/ShinyMACKLinear/app.R
+++ Shiny/ShinyMACKLinear/app.R
@@ -15,6 +15,13 @@ library(shinyjs)
 library(rsconnect)
 library(macklinear)
 
+#DATA
+data("cars")
+n = 300
+mydata <- cars
+y <- mydata$dist
+
+
 # Define UI
 ui <- fluidPage(theme = shinytheme("cosmo"),
 
@@ -128,9 +135,29 @@ ui <- fluidPage(theme = shinytheme("cosmo"),
                                        h2("Loading"),
 
 
+                                       sidebarLayout(
+                                         sidebarPanel(
+                                           sliderInput("alpha", "Alpha value:",
+                                                       value = 0.05,
+                                                       min = 0.01,
+                                                       max = 0.1),
+                                           br(),
+                                           br(),
+                                           radioButtons("method","Method:",
+                                                        c("asymptotic" = "Asymptoic",
+                                                          "bootstrap" = "Bootstrap")),
+                                           br(),
+                                           br(),
+                                           actionButton("button", "Show graph"),
+                                         ),
 
 
-
+                                         # Show a plot:
+                                         mainPanel(
+                                           plotOutput("FinalPlot")
+                                         )
+                                       )
+                              ),
 
 
 
@@ -139,7 +166,7 @@ ui <- fluidPage(theme = shinytheme("cosmo"),
 
 
 
-)
+
 )
 )
 #As tres ou duas ultimas de sempre que cada hora eh uma coisa
@@ -151,27 +178,17 @@ ui <- fluidPage(theme = shinytheme("cosmo"),
 
 
 # Define server function
-server <- function(input, output, session) {
+server <- function(input, output) {
 
-  # Combine the selected variables into a new data frame
-  selectedData <- reactive({
-    iris[, c(input$xcol, input$ycol)]
-  })
 
-  clusters <- reactive({
-    kmeans(selectedData(), input$clusters)
-  })
+    fit_my_lm <- my_lm(mydata$dist,myData,
+                       alpha = input$alpha, method = input$method,
+                       intercept = 1)
 
-  output$plot1 <- renderPlot({
-    palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
-              "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
 
-    par(mar = c(5.1, 4.1, 0, 1))
-    plot(selectedData(),
-         col = clusters()$cluster,
-         pch = 20, cex = 3)
-    points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
-  })
+    output$FinalPlot = renderPlot({
+      plot(fit_my_lm)
+    })
 
 
 
